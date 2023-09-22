1534b472e28fa748c217c3f164caa68a352a7694
Knoxianes <strahinja2001@gmail.com>
2023-09-22 22:52:54 +0200

    plugins

--- lua/knoxianes/init.lua
+++ lua/knoxianes/init.lua
@@ -2,67 +2,76 @@ require("knoxianes.remap")
 require("knoxianes.set")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	{"ThePrimeagen/vim-be-good",url="https://github.com/ThePrimeagen/vim-be-good"},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 999,
-		opts = {},
-	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-	{"ThePrimeagen/harpoon",ulr="https://github.com/ThePrimeagen/harpoon"},
-	{"mbbill/undotree",url="https://github.com/mbbill/undotree"},
-	{"tpope/vim-fugitive",url="https://github.com/tpope/vim-fugitive"},
-	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		dependencies = {
-			-- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+    { "ThePrimeagen/vim-be-good",        url = "https://github.com/ThePrimeagen/vim-be-good" },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 999,
+        opts = {},
+    },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "ThePrimeagen/harpoon",            ulr = "https://github.com/ThePrimeagen/harpoon" },
+    { "mbbill/undotree",                 url = "https://github.com/mbbill/undotree" },
+    { "tpope/vim-fugitive",              url = "https://github.com/tpope/vim-fugitive" },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },           -- Required
+            { 'williamboman/mason.nvim' },         -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
-            {'rafamadriz/friendly-snippets'},
-		}
-	},
-	{"windwp/nvim-ts-autotag",url="https://github.com/windwp/nvim-ts-autotag"},
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    },
+    { "windwp/nvim-ts-autotag",      url = "https://github.com/windwp/nvim-ts-autotag" },
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
         opts = {} -- this is equalent to setup({}) function
     },
-    {"nvim-tree/nvim-web-devicons",url="https://github.com/nvim-tree/nvim-web-devicons"},
-    {"nvim-lualine/lualine.nvim",url="https://github.com/nvim-lualine/lualine.nvim"},
-    {"lewis6991/gitsigns",url="https://github.com/lewis6991/gitsigns.nvim"},
-   
+    { "nvim-tree/nvim-web-devicons", url = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { "lewis6991/gitsigns",          url = "https://github.com/lewis6991/gitsigns.nvim" },
+
     {
         'numToStr/Comment.nvim',
         opts = {
             -- add any options here
         },
         lazy = false,
+    },
+    {
+        "roobert/tailwindcss-colorizer-cmp.nvim",
+        -- optionally, override the default options:
+        config = function()
+            require("tailwindcss-colorizer-cmp").setup({
+                color_square_width = 2,
+            })
+        end
     },
 })
