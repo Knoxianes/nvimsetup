51b68c426f9de5f607ff4406ab74b47e70f5d792
Strahinja <strahinja2001@gmail.com>
2023-10-12 19:54:01 +0200

    ...

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -4,4 +4,13 @@ lspconfig.gopls.setup{
     cmd = {"gopls"},
     filetyupes = {"go","gomod","gowork","gotmpl"},
     root_dir = util.root_pattern("go.work","go.mod",".git"),
+    settings = {
+        gopls={
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses ={
+                unusedparams = true,
+            },
+        },
+    },
 }
