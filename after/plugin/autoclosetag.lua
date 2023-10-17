9481b4ef8f9f61313cb59048e2595a34d4ba3665
Strahinja <strahinja2001@gmail.com>
2023-10-17 20:06:37 +0200

    ...

--- after/plugin/autoclosetag.lua
+++ after/plugin/autoclosetag.lua
@@ -1,3 +1,12 @@
-require('nvim-ts-autotag').setup(
-)
+local filetypes = {
+    'html', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'tsx', 'jsx', 'rescript',
+    'xml',
+    'php',
+    'markdown',
+    'astro', 'glimmer', 'handlebars', 'hbs',
+    "go.tmpl","go.txt","go.tpl"
+}
+require('nvim-ts-autotag').setup({
+    filetypes = filetypes,
+})
 
