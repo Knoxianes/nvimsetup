1afa755dc8d0621dd0797a1805271517d6e6516f
knoxianes <strahinja2001@gmail.com>
2024-01-13 13:24:47 +0100

    ...

--- lua/knoxianes/init.lua
+++ lua/knoxianes/init.lua
@@ -174,6 +174,22 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  -- Debugger
+  {
+    'mfussenegger/nvim-dap',
+  },
+  {
+  'leoluz/nvim-dap-go',
+  },
+  {
+  'rcarriga/nvim-dap-ui',
+  },
+  {
+  'theHamsta/nvim-dap-virtual-text',
+  },
+  {
+  'nvim-telescope/telescope-dap.nvim'
+  }
 
 }, {})
 
