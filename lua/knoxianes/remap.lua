f1bc7400b0d86cb28a608d297d41511dfb75e36d
knoxianes <strahinja2001@gmail.com>
2024-01-23 16:26:09 +0100

    added go err remap

--- lua/knoxianes/remap.lua
+++ lua/knoxianes/remap.lua
@@ -11,9 +11,9 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-vim.keymap.set({"i","v"}, "<C-c>", "<Esc>")
+vim.keymap.set({ "i", "v" }, "<C-c>", "<Esc>")
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format,{desc="LSP format"})
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "LSP format" })
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -30,9 +30,17 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Disable arrow keys
-vim.keymap.set({'n','v','i'},'<Left>','<Nop>')
-vim.keymap.set({'n','v','i'},'<Right>','<Nop>')
-vim.keymap.set({'n','v','i'},'<Down>','<Nop>')
-vim.keymap.set({'n','v','i'},'<Up>','<Nop>')
-vim.keymap.set({'n','v','i'},'<PageDown>','<Nop>')
-vim.keymap.set({'n','v','i'},'<PageUp>','<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<Left>', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<Right>', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<Down>', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<Up>', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<PageDown>', '<Nop>')
+vim.keymap.set({ 'n', 'v', 'i' }, '<PageUp>', '<Nop>')
+
+vim.keymap.set(
+  "n",
+  "<leader>ee",
+  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+)
+
+
