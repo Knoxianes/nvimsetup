aa88d978a451dbf99b7d6d10b6dd61f3b7d651d5
Strahinja Peric <knoxianes@Strahinjas-MacBook-Air.local>
2024-10-04 23:14:15 +0200

    fixed live grep

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -59,6 +59,29 @@ return {
         pcall(require('telescope').load_extension, 'fzf')
         pcall(require('telescope').load_extension, 'ui-select')
 
+        local function find_git_root()
+            -- Use the current buffer's path as the starting point for the git search
+            local current_file = vim.api.nvim_buf_get_name(0)
+            local current_dir
+            local cwd = vim.fn.getcwd()
+            -- If the buffer is not associated with a file, return nil
+            if current_file == "" then
+                current_dir = cwd
+            else
+                -- Extract the directory from the current file's path
+                current_dir = vim.fn.fnamemodify(current_file, ":h")
+            end
+
+            -- Find the Git root directory from the current file's path
+            local git_root = vim.fn.systemlist("git -C " ..
+            vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
+            if vim.v.shell_error ~= 0 then
+                print("Not a git repository. Searching on current working directory")
+                return cwd
+            end
+            return git_root
+        end
+
         local function live_grep_git_root()
             local git_root = find_git_root()
             if git_root then
@@ -91,19 +114,5 @@ return {
         vim.keymap.set('n', '<leader>pG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
         vim.keymap.set('n', '<leader>pd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>pr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-
-        -- It's also possible to pass additional configuration options.
-        --  See `:help telescope.builtin.live_grep()` for information about particular keys
-        vim.keymap.set('n', '<leader>s/', function()
-            builtin.live_grep {
-                grep_open_files = true,
-                prompt_title = 'Live Grep in Open Files',
-            }
-        end, { desc = '[S]earch [/] in Open Files' })
-
-        -- Shortcut for searching your Neovim configuration files
-        vim.keymap.set('n', '<leader>sn', function()
-            builtin.find_files { cwd = vim.fn.stdpath 'config' }
-        end, { desc = '[S]earch [N]eovim files' })
     end,
 }
